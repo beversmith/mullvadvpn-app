bc953eb592e23d94a2537a445a4b3709dc31b433
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-09-03 16:59:11 -0300

    Add authentication failure reason

--- talpid-core/src/tunnel/mod.rs
+++ talpid-core/src/tunnel/mod.rs
@@ -76,7 +76,7 @@ error_chain!{
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub enum TunnelEvent {
     /// Sent when the tunnel fails to connect due to an authentication error.
-    AuthFailed,
+    AuthFailed(Option<String>),
     /// Sent when the tunnel comes up and is ready for traffic.
     Up(TunnelMetadata),
     /// Sent when the tunnel goes down.
@@ -102,7 +102,10 @@ impl TunnelEvent {
         env: &HashMap<String, String>,
     ) -> Option<TunnelEvent> {
         match *event {
-            OpenVpnPluginEvent::AuthFailed => Some(TunnelEvent::AuthFailed),
+            OpenVpnPluginEvent::AuthFailed => {
+                let reason = env.get("auth_failed_reason").cloned();
+                Some(TunnelEvent::AuthFailed(reason))
+            }
             OpenVpnPluginEvent::Up => {
                 let interface = env
                     .get("dev")
